91dce81ad2129a489915d5fb1e876e1b443db21a
Juneed Raj Baba <babajuneed@Juneeds-MacBook-Pro.local>
2024-12-27 21:57:30 +0530

    Added all line to the script

--- generate_ASCII_Artwork.sh
+++ generate_ASCII_Artwork.sh
@@ -5,3 +5,8 @@ cat advice.json
 
 # Test to make sure the advice message has more than 5 words.
 cat advice.json | jq -r .slip.advice > advice.message
+[ $(wc -w < advice.message) -gt 5 ] && echo "Advice has more than 5 words" || (echo "Advice - $(cat advice.message) has 5 words")
+
+# Deploy
+sudo apt-get install cowsay -y
+cat advice.message | cowsay -f $(ls /usr/share/cowsay/cows | shuf -n 1)
