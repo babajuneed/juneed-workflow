9daf98c73fb37dbd139f05bf14f484e811923bb8
babajuneed <babajuneed@gmail.com>
2024-12-28 10:57:34 +0530

    Added the missing or less && exit 1 line

--- generate_ASCII_Artwork.sh
+++ generate_ASCII_Artwork.sh
@@ -5,7 +5,7 @@ cat advice.json
 
 # Test to make sure the advice message has more than 5 words.
 cat advice.json | jq -r .slip.advice > advice.message
-[ $(wc -w < advice.message) -gt 5 ] && echo "Advice has more than 5 words" || (echo "Advice - $(cat advice.message) has 5 words")
+[ $(wc -w < advice.message) -gt 5 ] && echo "Advice has more than 5 words" || (echo "Advice - $(cat advice.message) has 5 words or less" && exit 1)
 
 # Deploy
 sudo apt-get install cowsay -y
